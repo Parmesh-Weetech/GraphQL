004eb24d3b75f74032ad8c9eef0610f6b0ea00c9
Parmesh Bhatt <parmesh@weetechsolution.com>
2026-04-15 16:19:48 +0530

    feat: add rate limiting to graphql endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    '/graphql',
+    rateLimit({
+      windowMs: 60 * 1000,
+      max: 1,
+      message: 'Too many requests, try again after 5 minutes...',
+      standardHeaders: true
+    })
+  )
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap()
